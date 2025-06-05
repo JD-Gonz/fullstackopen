8b02ce60593b6545ff331de0abbd9d4e2802d11a
JD Gonzalez <jodg509@gmail.com>
2025-06-04 19:13:35 -0700

    1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p><strong>Good</strong>: {props.good}  | <strong>Neutral</strong>: {props.neutral}  | <strong>Bad</strong>: {props.bad}</p>
+      <p>All: {props.total}</p>
+      <p>Average: {props.total > 0 ? (props.good - props.bad) / (props.total) : 0}</p>
+      <p>Positive: {props.total > 0 ? (props.good / (props.total) * 100) : 0}%</p>
+    </>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -15,12 +27,7 @@ function App() {
     <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
     <button onClick={() => setBad(bad + 1)}>Bad</button>
 
-    <h2>Statistics</h2>
-
-    <p><strong>Good</strong>: {good}  | <strong>Neutral</strong>: {neutral}  | <strong>Bad</strong>: {bad}</p>
-    <p>All: {total}</p>
-    <p>Average: {total > 0 ? (good - bad) / (total) : 0}</p>
-    <p>Positive: {total > 0 ? (good / (total) * 100) : 0}%</p>
+    <Statistics good={good} neutral={neutral} bad={bad} total={total} />
   </>
 }
 
